4659ae510c5ea3c1ae734a23c110251a9b2863c1
X极限战士X <1014723833@qq.com>
2018-12-04 22:40:50 +0800

    添加项目文件。

---
+++ ObjectExplorer-UPM-Besiege/Mod.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PluginManager.Plugin;
+using UnityEngine;
+
+namespace ObjectExplore
+{
+
+    ///  https://github.com/spaar/besiege-modloader
+
+
+    [OnGameInit]
+    public class Mod : MonoBehaviour
+    {
+        GameObject mod = new GameObject("Object Explore - UPM Edition");
+        
+        void Start()
+        {
+            mod.AddComponent<ObjectExplorerMod.ObjectExplorer>();
+            DontDestroyOnLoad(mod);
+        }
+
+    }
+}
---
+++ ObjectExplorer-UPM-Besiege/ObjectExplorer.cs
@@ -0,0 +1,60 @@
+﻿using ObjectExplorerMod.UI;
+using UnityEngine;
+using Modding;
+
+namespace ObjectExplorerMod
+{
+    public class ObjectExplorer : SingleInstance<ObjectExplorer>
+    {
+        public override string Name
+        {
+            get { return "Object Explorer - UPM Edition"; }
+        }
+
+        public string WindowTitle = "Object Explorer - UPM Edition";
+
+        public HierarchyPanel HierarchyPanel { get; private set; }
+        public InspectorPanel InspectorPanel { get; private set; }
+
+        private readonly int windowID = Modding.ModUtility.GetWindowId();
+        private Rect windowRect = new Rect(20, 20, 800, 600);
+        private MKey key;
+
+        public bool IsVisible = false;
+
+        private void Start()
+        {
+            HierarchyPanel = gameObject.AddComponent<HierarchyPanel>();
+            InspectorPanel = gameObject.AddComponent<InspectorPanel>();
+            key = new MKey("Show", "Show", KeyCode.O);
+        }
+
+        private void Update()
+        {
+            if (key.IsPressed)
+            {
+                IsVisible = !IsVisible;
+            }
+        }
+
+        private void OnGUI()
+        {
+            GUI.skin = ModGUI.Skin;
+
+            if (IsVisible) { windowRect = GUILayout.Window(windowID, windowRect, DoWindow, WindowTitle); }
+        }
+
+        private void DoWindow(int id)
+        {
+            GUILayout.BeginHorizontal();
+
+            HierarchyPanel.Display();
+            InspectorPanel.Display();
+
+            GUILayout.EndHorizontal();
+
+            GUI.DragWindow(new Rect(0, 0, windowRect.width, GUI.skin.window.padding.top));
+        }
+    }
+}
+
